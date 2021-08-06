16abcaf29f3f226b4950e5f86768f3a43c30c89d
OnyemaAnthony <Anthonychibuike246@gmail.com>
2021-08-06 21:10:27 +0000

    finish styling body

--- src/componets/Chat.js
+++ src/componets/Chat.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../css/Chat.css'
+import {Avatar, IconButton} from "@material-ui/core";
+import {AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined} from "@material-ui/icons";
 
 const Chat = () => {
+    const [seed, setSeed] = useState(0);
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, [])
+
     return (
         <div className='chat'>
 
+
+            <div className="chat_header">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                <div className="chat_headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at ..</p>
+                </div>
+
+                <div className="chat_headerRight">
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
+
+            </div>
+
+            <div className="chat_body">
+                <p className={`chat_message chat_receiver `}>
+                    <span className='chat_name'> Anthony</span>
+                    Hey gus
+
+                    <span className='chat_timestamp'>3:53pm</span>
+
+                </p>
+
+            </div>
+            <div className="chat_footer">
+                <InsertEmoticon/>
+                <form >
+                    <input placeholder='Type a message' type="text"/>
+                    <button>Send message</button>
+                </form>
+                <Mic/>
+            </div>
         </div>
+
     );
 };
 
